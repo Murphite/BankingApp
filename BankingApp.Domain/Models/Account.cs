9598472e860e0db61c5e85221a23e72882b33c23
Murphy Ogbeide <61960212+Murphite@users.noreply.github.com>
2025-08-08 11:24:03 +0100

    Add project files.

---
+++ BankingApp.Domain/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BankingApp.Domain.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public string OwnerId { get; set; } // foreign key to User
+        public List<Transaction> Transactions { get; set; }
+    }
+}
---
+++ BankingApp.Domain/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace BankingApp.Domain.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Type { get; set; } // Deposit, Withdraw, Transfer
+        public string Description { get; set; }
+    }
+
+}
